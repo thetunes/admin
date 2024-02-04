ea14b90fb46e9a12632bebb21e49b849bb827261
HeroBuxx <me@herobuxx.me>
2024-02-04 22:25:24 +0700

    admin: Make order detail more human readable

--- src/js/order-detail.js
+++ src/js/order-detail.js
@@ -37,6 +37,28 @@ function getDetail() {
         });
 }
 
+function getTicketTitle(ticketId) {
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+    return fetch(`http://34.128.102.98/api/ticket/${ticketId}`, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === "success") {
+                return data.data.title;
+            } else {
+                console.error('API Error:', data.message);
+                return null;
+            }
+        })
+        .catch(error => {
+            console.error('Fetch Error:', error);
+            return null;
+        });
+}
+
 function confirmOrder(orderId) {
     var requestOptions = {
         method: 'POST',
@@ -96,127 +118,129 @@ function displayDetail(order) {
 
     const data = document.createElement('div');
 
-    if (order.status === "true") {
-        data.innerHTML = `
-            <div id="cards-body" class="bg-gray-900 w-full py-6 px-8 my-8 rounded-2xl">
-            <h1 class="font-extrabold text-3xl">Order Details</h1>
+    // Fetch ticket title
+    getTicketTitle(order.ticketid).then(ticketTitle => {
+        if (order.status === "true") {
+            data.innerHTML = `
+                <div id="cards-body" class="bg-gray-900 w-full py-6 px-8 my-8 rounded-2xl">
+                <h1 class="font-extrabold text-3xl">Order Details</h1>
 
-            <div class="my-8">
-                <div class="relative overflow-x-auto py-6">
-                    <h3 class="font-extrabold text-2xl">Customer ID</h3>
-                    <span>${order.userid}</span>
-                </div>
+                <div class="my-8">
+                    <div class="relative overflow-x-auto py-6">
+                        <h3 class="font-extrabold text-2xl">Customer ID</h3>
+                        <span>${order.userid}</span>
+                    </div>
 
-                <div class="relative overflow-x-auto py-6">
-                    <h3 class="font-extrabold text-2xl">Ticket ID</h3>
-                    <span>${idValue}</span>
-                </div>
+                    <div class="relative overflow-x-auto py-6">
+                        <h3 class="font-extrabold text-2xl">Event Title</h3>
+                        <span>${ticketTitle}</span>
+                    </div>
 
-                <div class="relative overflow-x-auto py-6">
-                    <h3 class="font-extrabold text-2xl">Ordered At</h3>
-                    <span>${order.CreatedAt}</span>
+                    <div class="relative overflow-x-auto py-6">
+                        <h3 class="font-extrabold text-2xl">Ordered At</h3>
+                        <span>${order.CreatedAt}</span>
+                    </div>
+                    <div class="relative overflow-x-auto py-6">
+                        <h3 class="font-extrabold text-2xl">Status</h3>
+                        <span>Complete</span>
+                    </div>
+                    <div class="relative overflow-x-auto py-6">
+                        <h3 class="font-extrabold text-2xl">Payment Receipt</h3>
+                        <a href="http://34.128.102.98/api/order/show/payment?file=${idValue}.jpg">
+                            <img class="w-auto h-80" src="http://34.128.102.98/api/order/show/payment?file=${idValue}.jpg">
+                        </a>
+                    </div>
                 </div>
                 <div class="relative overflow-x-auto py-6">
-                    <h3 class="font-extrabold text-2xl">Status</h3>
-                    <span>Complete</span>
-                </div>
-                <div class="relative overflow-x-auto py-6">
-                    <h3 class="font-extrabold text-2xl">Payment Receipt</h3>
-                    <a href="http://34.128.102.98/api/order/show/payment?file=${idValue}.jpg">
-                        <img class="w-auto h-80" src="http://34.128.102.98/api/order/show/payment?file=${idValue}.jpg">
-                    </a>
+                    <div class="text-center">
+                        <button onClick='cancelOrder("${order.id}")' class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full w-1/3">
+                            Cancel
+                        </button>    
+                    </div>
                 </div>
             </div>
-            <div class="relative overflow-x-auto py-6">
-                <div class="text-center">
-                    <button onClick='cancelOrder("${order.id}")' class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full w-1/3">
-                        Cancel
-                    </button>    
+            `;
+        } else if (order.status === "false") {
+            data.innerHTML = `
+                <div id="cards-body" class="bg-gray-900 w-full py-6 px-8 my-8 rounded-2xl">
+                <h1 class="font-extrabold text-3xl">Order Details</h1>
+
+                <div class="my-8">
+                    <div class="relative overflow-x-auto py-6">
+                        <h3 class="font-extrabold text-2xl">Customer ID</h3>
+                        <span>${order.userid}</span>
+                    </div>
+
+                    <div class="relative overflow-x-auto py-6">
+                        <h3 class="font-extrabold text-2xl">Event Title</h3>
+                        <span>${ticketTitle}</span>
+                    </div>
+
+                    <div class="relative overflow-x-auto py-6">
+                        <h3 class="font-extrabold text-2xl">Ordered At</h3>
+                        <span>${order.CreatedAt}</span>
+                    </div>
+                    <div class="relative overflow-x-auto py-6">
+                        <h3 class="font-extrabold text-2xl">Status</h3>
+                        <span>Cancelled</span>
+                    </div>
+                    <div class="relative overflow-x-auto py-6">
+                        <h3 class="font-extrabold text-2xl">Payment Receipt</h3>
+                        <a href="http://34.128.102.98/api/order/show/payment?file=${idValue}.jpg">
+                            <img class="w-auto h-80" src="http://34.128.102.98/api/order/show/payment?file=${idValue}.jpg">
+                        </a>
+                    </div>                
                 </div>
             </div>
-        </div>
-        `;
-    } else if (order.status === "false") {
-        data.innerHTML = `
-            <div id="cards-body" class="bg-gray-900 w-full py-6 px-8 my-8 rounded-2xl">
-            <h1 class="font-extrabold text-3xl">Order Details</h1>
+            `;
+        } else {
+            data.innerHTML = `
+                <div id="cards-body" class="bg-gray-900 w-full py-6 px-8 my-8 rounded-2xl">
+                <h1 class="font-extrabold text-3xl">Order Details</h1>
 
-            <div class="my-8">
-                <div class="relative overflow-x-auto py-6">
-                    <h3 class="font-extrabold text-2xl">Customer ID</h3>
-                    <span>${order.userid}</span>
-                </div>
+                <div class="my-8">
+                    <div class="relative overflow-x-auto py-6">
+                        <h3 class="font-extrabold text-2xl">Customer ID</h3>
+                        <span>${order.userid}</span>
+                    </div>
 
-                <div class="relative overflow-x-auto py-6">
-                    <h3 class="font-extrabold text-2xl">Ticket ID</h3>
-                    <span>${idValue}</span>
-                </div>
+                    <div class="relative overflow-x-auto py-6">
+                        <h3 class="font-extrabold text-2xl">Event Title</h3>
+                        <span>${ticketTitle}</span>
+                    </div>
 
-                <div class="relative overflow-x-auto py-6">
-                    <h3 class="font-extrabold text-2xl">Ordered At</h3>
-                    <span>${order.CreatedAt}</span>
-                </div>
-                <div class="relative overflow-x-auto py-6">
-                    <h3 class="font-extrabold text-2xl">Status</h3>
-                    <span>Cancelled</span>
-                </div>
-                <div class="relative overflow-x-auto py-6">
-                    <h3 class="font-extrabold text-2xl">Payment Receipt</h3>
-                    <a href="http://34.128.102.98/api/order/show/payment?file=${idValue}.jpg">
-                        <img class="w-auto h-80" src="http://34.128.102.98/api/order/show/payment?file=${idValue}.jpg">
-                    </a>
-                </div>                
-            </div>
-        </div>
-        `;
-    } else {
-        data.innerHTML = `
-            <div id="cards-body" class="bg-gray-900 w-full py-6 px-8 my-8 rounded-2xl">
-            <h1 class="font-extrabold text-3xl">Order Details</h1>
+                    <div class="relative overflow-x-auto py-6">
+                        <h3 class="font-extrabold text-2xl">Ordered At</h3>
+                        <span>${order.CreatedAt}</span>
+                    </div>
+                    <div class="relative overflow-x-auto py-6">
+                        <h3 class="font-extrabold text-2xl">Status</h3>
+                        <span>Need to confitm</span>
+                    </div>
+                    <div class="relative overflow-x-auto py-6">
+                        <h3 class="font-extrabold text-2xl">Payment Receipt</h3>
+                        <a href="http://34.128.102.98/api/order/show/payment?file=${idValue}.jpg">
+                            <img class="w-auto h-80" src="http://34.128.102.98/api/order/show/payment?file=${idValue}.jpg">
+                        </a>
+                    </div>
 
-            <div class="my-8">
-                <div class="relative overflow-x-auto py-6">
-                    <h3 class="font-extrabold text-2xl">Customer ID</h3>
-                    <span>${order.userid}</span>
                 </div>
+                <div class="relative overflow-x-auto py-6">
+                    <div class="text-center">
+                        <button onClick='confirmOrder("${order.id}")' class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-1/3">
+                            Confirm
+                        </button>    
 
-                <div class="relative overflow-x-auto py-6">
-                    <h3 class="font-extrabold text-2xl">Ticket ID</h3>
-                    <span>${idValue}</span>
-                </div>
-
-                <div class="relative overflow-x-auto py-6">
-                    <h3 class="font-extrabold text-2xl">Ordered At</h3>
-                    <span>${order.CreatedAt}</span>
-                </div>
-                <div class="relative overflow-x-auto py-6">
-                    <h3 class="font-extrabold text-2xl">Status</h3>
-                    <span>Need to confitm</span>
-                </div>
-                <div class="relative overflow-x-auto py-6">
-                    <h3 class="font-extrabold text-2xl">Payment Receipt</h3>
-                    <a href="http://34.128.102.98/api/order/show/payment?file=${idValue}.jpg">
-                        <img class="w-auto h-80" src="http://34.128.102.98/api/order/show/payment?file=${idValue}.jpg">
-                    </a>
-                </div>
-
-            </div>
-            <div class="relative overflow-x-auto py-6">
-                <div class="text-center">
-                    <button onClick='confirmOrder("${order.id}")' class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-1/3">
-                        Confirm
-                    </button>    
-
-                    <button onClick='cancelOrder("${order.id}")' class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full w-1/3">
-                        Cancel
-                    </button>    
+                        <button onClick='cancelOrder("${order.id}")' class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full w-1/3">
+                            Cancel
+                        </button>    
+                    </div>
                 </div>
             </div>
-        </div>
-        `;
-    }
-
-    tableBody.appendChild(data);
+            `;
+        }
+        tableBody.appendChild(data);
+    });
 }
 
 function loadData() {
